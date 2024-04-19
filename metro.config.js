c5d2193e86ffb56258403a20acb512d47e22b011
oleg shchepotkin <music.wave.form7@gmail.com>
2024-04-19 19:20:56 +0300

    (env): init setup

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,6 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push('db');
 
 module.exports = config;
---
+++ services/data.layer.ts
@@ -0,0 +1,21 @@
+import * as FileSystem from 'expo-file-system';
+import * as SQLite from 'expo-sqlite';
+import { Asset } from 'expo-asset';
+const shopDataBase = require('../assets/db/shop.db');
+
+
+
+
+export class DataLayer {
+    static async asyncopenDatabase(): Promise<SQLite.SQLiteDatabase> {
+        if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+          await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+        }
+        const asset = await Asset.fromModule(shopDataBase).downloadAsync();
+        await FileSystem.copyAsync({
+          from: asset.localUri!,
+          to: FileSystem.documentDirectory + 'SQLite/myDatabaseName.db',
+        });
+        return SQLite.openDatabase('myDatabaseName.db');
+      }    
+}
