632a23a989683fe4ceb268f1e0786f90a3ea833f
oleg shchepotkin <music.wave.form7@gmail.com>
2024-04-20 19:37:51 +0300

    (data leyer): additional admin path

---
+++ services/data/order.itm.dl.ts
@@ -0,0 +1,59 @@
+import { DataLayer } from './util.dl';
+
+export interface OrderItem {
+    id: number;
+    orderId: number;
+    productId: number;
+    productName: string;
+}
+
+
+export class OrderItems {
+   async addOrderItem(orderItem: OrderItem): Promise<OrderItem> {
+    const db = await DataLayer.asyncopenDatabase();
+    const { orderId, productId, productName } = orderItem;
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'INSERT INTO order_items (orderId, productId, productName) VALUES (?, ?, ?)',
+          [orderId, productId, productName],
+          (_, result) => {
+            if (result.rowsAffected > 0) {
+              resolve(orderItem);
+            } else {
+              reject(new Error('Failed to make order'));
+            }
+          },
+          (_, error) => {
+            reject(error);
+            return true;
+          }
+        );
+      });
+    });
+  }
+
+  async listOrderItems(orderId: number): Promise<OrderItem[]> {
+    const db = await DataLayer.asyncopenDatabase();
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT * FROM order_items WHERE orderId = ?',
+          [orderId],
+          (_, { rows }) => {
+            const orders: OrderItem[] = [];
+            for (let i = 0; i < rows.length; i++) {
+              const { id, orderId, productId, productName } = rows.item(i);
+              orders.push({ id, orderId, productId, productName });
+            }
+            resolve(orders);
+          },
+          (_, error) => {
+            reject(error);
+            return true;
+          }
+        );
+      });
+    });
+  }
+}
---
+++ services/data/orders.dl.ts
@@ -0,0 +1,59 @@
+import { DataLayer } from './util.dl';
+
+export interface Order {
+  id: number;
+  userId: number;
+  totalPrice: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export class Orders{
+   async findAll(): Promise<Order[]> {
+    const db = await DataLayer.asyncopenDatabase();
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'SELECT * FROM orders',
+          [],
+          (_, { rows }) => {
+            const orders: Order[] = [];
+            for (let i = 0; i < rows.length; i++) {
+              const { id, userId, totalPrice, firstName, lastName, email } = rows.item(i);
+              orders.push({ id, userId, totalPrice, firstName, lastName, email });
+            }
+            resolve(orders);
+          },
+          (_, error) => {
+            reject(error);
+            return true;
+          }
+        );
+      });
+    });
+  }
+  async addOrder(order: Order): Promise<Order> {
+    const db = await DataLayer.asyncopenDatabase();
+    const { userId, totalPrice, firstName, lastName, email } = order;
+    return new Promise((resolve, reject) => {
+      db.transaction(tx => {
+        tx.executeSql(
+          'INSERT INTO order_items (userId, totalPrice, firstName, lastName, email) VALUES (?, ?, ?, ?, ?)',
+          [userId, totalPrice, firstName, lastName, email],
+          (_, result) => {
+            if (result.rowsAffected > 0) {
+              resolve(order);
+            } else {
+              reject(new Error('Failed to make order'));
+            }
+          },
+          (_, error) => {
+            reject(error);
+            return true;
+          }
+        );
+      });
+    });
+  }
+}
