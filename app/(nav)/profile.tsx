3985bf721d44fb6ffd10795521c9f832aeadac5e
oleg shchepotkin <music.wave.form7@gmail.com>
2024-04-22 17:06:46 +0300

    (profile): profile design

--- app/(nav)/profile.tsx
+++ app/(nav)/profile.tsx
@@ -1,83 +1,53 @@
-import React, { useState } from 'react';
-import {
-	Text,
-	KeyboardAvoidingView,
-	Platform,
-	StyleSheet,
-	TextInput,
-	TouchableOpacity,
-    Button,
-    View
-} from 'react-native';
-import { useAuth } from '../../context/AuthContext';
 import { router } from 'expo-router';
+import { Text, View, Input, Button, Card, Separator } from 'tamagui';
+import { useAuth } from '~/context/AuthContext';
+import { Title } from '~/tamagui.config';
 
 const Page = () => {
-	const [username, setUsername] = useState('admin');
-	const [password, setPassword] = useState('admin');
-	const { onLogin, authState, onLogout } = useAuth();
+
+	const { authState, onLogout } = useAuth();
 
 	const onLogoutPressed = () => {
 		onLogout!();
 		router.replace('/');
 	};
 
-	const onSignInPressUser = async () => {
-		onLogin!('user', 'user');
-	};
-
-	const onSignInPressAdmin = async () => {
-		onLogin!('admin', 'admin');
-	};
-
 	return (
-          <View style={styles.container}>
-			<Text style={styles.title}>Profile:</Text>
-			<Text style={styles.title}>Name: {authState?.user?.username}</Text>
-			<Text style={styles.title}>Email: {authState?.user?.email}</Text>
-			<Text style={styles.title}>Role: {authState?.role}</Text>
-			<Button title="Logout" onPress={onLogoutPressed} />
-			<View style={styles.separator} />
-		  </View>
-        )
-};
+		<View
+			flexDirection="column"
+			alignItems='center'
+			justifyContent='center'
+			height={'100%'}
+			width={'100%'}
+		> 
+			<Title
+				mt={10}
+				animation="quick">
+				Profile details:
+			</Title>
+			<Separator width={'80%'} marginVertical={25} />
+			  <Card
+			  	width={'90%'}
+				height={330}
+			  	p={10}
+			  >
+				<Text mt={15} mb={8} color={'white'}>Name</Text>
+				<Input value={authState?.user?.username} readOnly />
+				<Text mt={15} mb={8} color={'white'}>Email</Text>
+				<Input value={authState?.user?.email} readOnly />
+				<Text mt={15} mb={8} color={'white'}>Role</Text>
+				<Input value={authState?.role} readOnly />
+			  </Card>
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 20,
-		paddingHorizontal: '20%',
-		justifyContent: 'center'
-	},
-	header: {
-		fontSize: 30,
-		textAlign: 'center',
-		marginBottom: 40
-	},
-	inputField: {
-		marginVertical: 4,
-		height: 50,
-		borderWidth: 1,
-		borderColor: '#ccc',
-		borderRadius: 4,
-		padding: 10
-	},
-	button: {
-		marginVertical: 15,
-		alignItems: 'center',
-		backgroundColor: '#111233',
-		padding: 12,
-		borderRadius: 4
-	},
-	separator: {
-		height: 1,
-		marginVertical: 30,
-		width: '80%'
-	},
-	title: {
-		fontSize: 20,
-		fontWeight: 'bold'
-	}
-});
+			  <Button
+			  	mt={30}
+			  	width={'90%'}
+			  	onPress={onLogoutPressed}
+			  >
+				Logout
+			  </Button>
+		</View>
+	);
+};
 
 export default Page;
