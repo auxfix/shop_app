4cade797459df335087e4d963f30ac107b09c644
oleg shchepotkin <music.wave.form7@gmail.com>
2024-04-23 13:26:27 +0300

    (test): api tests

---
+++ __tests__/services/api/cart.api.test.ts
@@ -0,0 +1,44 @@
+import { CartItem } from '~/services/data/cart.dl';
+import { fakeCartDl } from '~/services/data/fake/cart.fake.dl';
+import { CartApi } from '~/services/api/cart.api';
+
+const USER_ID = 1;
+
+const TEST_CART_ITEM: CartItem =  { 
+    price: 1.7,
+    productId: 5,
+    productName: 'Tomato',
+    userId: USER_ID,
+};
+
+const cartApiWithFakeCartDl = new CartApi(fakeCartDl);
+
+describe('Api -> Cart', () => {
+
+  it('Init Cart API should return fake data', async () => {
+    const fakeCartData = fakeCartDl.getFakeCart();
+    const fakeCartDataFromApi = await cartApiWithFakeCartDl.getCartByUserId(USER_ID);
+
+    expect(fakeCartData.length).toEqual(fakeCartDataFromApi.length);
+
+    expect(fakeCartData[0].productId).toEqual(fakeCartDataFromApi[0].productId);
+    expect(fakeCartData[0].productName).toEqual(fakeCartDataFromApi[0].productName);
+    expect(fakeCartData[0].userId).toEqual(fakeCartDataFromApi[0].userId);
+  });
+
+  it('Cart Api can not change fake DL cart', async () => {
+     await cartApiWithFakeCartDl.addToCart(TEST_CART_ITEM);
+    
+     const fakeCartDataFromApi = await cartApiWithFakeCartDl.getCartByUserId(USER_ID);
+     expect(fakeCartDataFromApi.length).toEqual(2);
+  });
+
+  it('Cart Api can clear fake DL cart', async () => {
+    await cartApiWithFakeCartDl.cleanCart(USER_ID);
+
+    const fakeCartData = fakeCartDl.getFakeCart();
+    const fakeCartDataFromApi = await cartApiWithFakeCartDl.getCartByUserId(USER_ID);
+    expect(fakeCartDataFromApi.length).toEqual(0);
+    expect(fakeCartData.length).toEqual(0);
+  });
+});
---
+++ services/data/fake/cart.fake.dl.ts
@@ -0,0 +1,38 @@
+import { CartDl, CartItem } from "~/services/data/cart.dl";
+
+export class CartFakeDl extends CartDl {
+
+  private fakeCart: CartItem[];
+  
+  constructor(){
+    super();
+    this.fakeCart = [
+      { 
+        price: 1.2,
+        productId: 1,
+        productName: 'Banana',
+        userId: 1,
+      },
+      { 
+        price: 1.4,
+        productId: 1,
+        productName: 'Potato',
+        userId: 2,
+      }
+    ]
+  }
+
+  getFakeCart() {
+    return this.fakeCart.slice();
+  }
+
+  async getCartByUserId(userId: number): Promise<CartItem[]> {
+    return this.fakeCart.slice();
+  }
+
+  async cleanCartByUserId(userId: number): Promise<void> {
+    this.fakeCart = [];
+  }
+}
+
+export const fakeCartDl = new CartFakeDl();
